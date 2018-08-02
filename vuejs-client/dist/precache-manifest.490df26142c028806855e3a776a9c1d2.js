fd799bcd756cc24d7324960f1eece3718c366724
Robert Gray <robert.gray@directski.com>
2018-08-02 17:24:36 +0100

    first production build

---
+++ vuejs-client/dist/precache-manifest.490df26142c028806855e3a776a9c1d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "de59a79b448cb7b34652",
+    "url": "/js/chunk-vendors.de59a79b.js"
+  },
+  {
+    "revision": "d959a8f9c31de05dc512",
+    "url": "/js/app.d959a8f9.js"
+  },
+  {
+    "revision": "3f8234bda5c22dcbcab0780bf908135a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0af4007b41b09ba08bbce9e652cd3f3",
+    "url": "/img/platypus.svg"
+  },
+  {
+    "revision": "de59a79b448cb7b34652",
+    "url": "/css/chunk-vendors.43369001.css"
+  },
+  {
+    "revision": "d959a8f9c31de05dc512",
+    "url": "/css/app.015a94c8.css"
+  }
+];
